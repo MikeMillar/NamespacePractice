21126fb1f82a83ef41b4ce262fe04f26a16ba573
Mike Millar <39285355+MikeMillar@users.noreply.github.com>
2019-07-18 15:09:35 -0600

    constructor inheritance

--- NamespacePractice/main.cpp
+++ NamespacePractice/main.cpp
@@ -9,7 +9,7 @@ public:
 	void speak() { cout << "Grrrr" << endl; };
 };
 
-class Dog: public Animal {
+class Dog : public Animal {
 public:
 	void bark() { cout << "Wooof!" << endl; }
 };
@@ -17,6 +17,37 @@ public:
 class Husky : public Dog {
 public:
 	void shed() { cout << "Furr is everywhere!" << endl; }
+};
+
+class Frog {	
+private: // Private variables are encapsulated away, so that you can't access its data from outside the class.
+	string name;
+	string getName() { return name; }
+public: // Public allows access to methods or data located in the public section
+	Frog(string name): name(name) {}
+	void info() { cout << "My name is: " << getName() << endl; }
+};
+
+class Machine {
+private:
+	int id;
+public:
+	Machine(): id(0) { cout << "Machine no-argument constructor called" << endl; }
+	Machine(int id): id(id) { cout << "Machine id argument constructor called" << endl; }
+	void info() { cout << "ID: " << id << endl; }
+};
+
+class Vehicle : public Machine {
+public:
+	Vehicle() { cout << "Vehicle no-argument constructor called" << endl; }
+	Vehicle(int id): Machine(id) { cout << "Vehicle id argument constructor called" << endl; }
+};
+
+class Car : public Vehicle {
+private:
+public:
+	Car(): Vehicle(999) { cout << "Car no-argument constructor called" << endl; }
+
 };
 
 int main() {
@@ -42,7 +73,23 @@ int main() {
 	c.bark();
 	c.shed();
 
+	Frog frog("Bob");
+	frog.info();
 
+	Machine m;
+	m.info();
+
+	Machine n(23);
+	n.info();
+
+	Vehicle v;
+	v.info();
+
+	Vehicle y(12);
+	y.info();
+
+	Car w;
+	w.info();
 
 	return 0;
 }
